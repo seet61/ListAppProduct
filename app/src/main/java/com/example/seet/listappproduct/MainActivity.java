2aeb86dc78dcd2d23aabc8fe93cbac1589d3864f
seet61 <seet_seet@mail.ru>
2017-05-15 12:15:50 +0300

    add adapter

--- app/src/main/java/com/example/seet/listappproduct/MainActivity.java
+++ app/src/main/java/com/example/seet/listappproduct/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.seet.listappproduct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    //Массив элементов для отображения
+    ArrayList<Product> products = new ArrayList<>();
+    ListView productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Инициализация при открытии
+        if (products.size() == 0) {
+            products.add(new Product("Картофель", "кг."));
+            products.add(new Product("Чай", "шт."));
+            products.add(new Product("Яйца", "шт."));
+            products.add(new Product("Молоко", "л."));
+            products.add(new Product("Макароны", "кг."));
+        }
+        productList = (ListView) findViewById(R.id.productList);
+        ProductAdapter adapter = new ProductAdapter(this, R.layout.list_item, products);
+        productList.setAdapter(adapter);
     }
 }
