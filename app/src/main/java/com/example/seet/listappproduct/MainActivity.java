cfacd8c9d380238cf132a4b3a38a6368d2b703d4
seet61 <seet_seet@mail.ru>
2017-05-15 13:54:25 +0300

    add onOptionsItemSelected(MenuItem item)

--- app/src/main/java/com/example/seet/listappproduct/MainActivity.java
+++ app/src/main/java/com/example/seet/listappproduct/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.seet.listappproduct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListView productList;
 
     //Данные для Spinner
-    String[] citites = {"Москва", "Самара", "Вологла", "Волгоград", "Саратов", "Воронеж", "Ростов-на-Дону"};
+    String[] citites = {"Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж", "Ростов-на-Дону"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +45,30 @@ public class MainActivity extends AppCompatActivity {
         productList = (ListView) findViewById(R.id.productList);
         ProductAdapter adapter = new ProductAdapter(this, R.layout.list_item, products);
         productList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = (TextView) findViewById(R.id.header);
+        switch (id){
+            case R.id.action_settings:
+                headerView.setText("Настройки");
+                break;
+            case R.id.open_action:
+                headerView.setText("Открыть");
+                break;
+            case R.id.save_settings:
+                headerView.setText("Сохранить");
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
